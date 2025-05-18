9830656e0f3cec0fa309e967e659fda4bfa4fbfa
Rayhon Irisqulova <rayhonirisqulova@gmail.com>
2025-05-18 15:21:21 +0500

    Some features changed

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -155,4 +158,7 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
 CELERYD_FORCE_EXECV = True
+
+
+
 
